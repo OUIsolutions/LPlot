4d0d62845632f8cbcbb94f074276110e06756211
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2026-04-04 06:06:38 -0300

    att

---
+++ builds/amalgamation.lua
@@ -0,0 +1,31 @@
+
+function amalgamate()
+
+    local project = darwin.create_project("lplot")
+    project.add_lua_code("private_lplot =  {}")
+    project.add_lua_code("lplot = {}")
+    project.add_lua_code("private_lua_argv_string_functions = {}")
+
+    local concat_path = true
+    local src_files = darwin.dtw.list_files_recursively("src", concat_path)
+    for i = 1, #src_files do
+        local current = src_files[i]
+        project.add_lua_code("-- file: " .. current .. "\n")
+        project.add_lua_file(current)
+    end
+    project.add_lua_code("return lplot")
+    project.generate_lua_file({
+        output = "release/lplot.lua"
+    })
+
+end 
+
+
+
+darwin.add_recipe({
+    name="amalgamation",
+    description="make a single file amalgamation of the project",
+    outs={"release/lplot.lua"},
+    inputs={"src"},
+    callback=amalgamate
+})
--- darwinconf.lua
+++ darwinconf.lua
@@ -1,17 +1,1 @@
-local project = darwin.create_project("lplot")
-project.add_lua_code("private_lplot =  {}")
-project.add_lua_code("lplot = {}")
-project.add_lua_code("private_lua_argv_string_functions = {}")
-
-local concat_path = true
-local src_files = darwin.dtw.list_files_recursively("src", concat_path)
-for i = 1, #src_files do
-    local current = src_files[i]
-    project.add_lua_code("-- file: " .. current .. "\n")
-    project.add_lua_file(current)
-end
-project.add_lua_code("return lplot")
-
-project.generate_lua_file({
-    output = "lplot.lua"
-})
+darwin.load_all("builds")
