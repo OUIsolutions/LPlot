5a97aa3199ba466810391f705ac51ca45a3e059d
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2026-04-04 07:34:52 -0300

    att

---
+++ src/publiclplot-types.lua
@@ -0,0 +1,11 @@
+
+--@class LPlotNotebook
+--@field add_text(text: string): LPlotNotebook
+--@field add_title(title: string): LPlotNotebook
+
+--@field get_html(): string
+
+
+
+--@class LplotModule
+--@field new_notebook fun(): LplotNotebook
