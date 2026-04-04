bcd248ce54c31d6d726428dc4f4e6e0b67320a23
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2026-04-04 07:30:48 -0300

    att

--- builds/amalgamation.lua
+++ builds/amalgamation.lua
@@ -2,18 +2,19 @@
 function amalgamation_build()
 
     local project = darwin.create_project("lplot")
-    project.add_lua_code("private_lplot =  {}")
-    project.add_lua_code("lplot = {}")
-    project.add_lua_code("private_lua_argv_string_functions = {}")
+    project.add_lua_code("return (function()")
+    project.add_lua_code("local private_lplot =  {}")
+    project.add_lua_code("local lplot = {}")
 
     local concat_path = true
-    local src_files = darwin.dtw.list_files_recursively("src", concat_path)
+    local src_files = darwin.dtw.list_files_recursively("src/functions", concat_path)
     for i = 1, #src_files do
         local current = src_files[i]
         project.add_lua_code("-- file: " .. current .. "\n")
         project.add_lua_file(current)
     end
     project.add_lua_code("return lplot")
+    project.add_lua_code("end)()")
     project.generate_lua_file({
         output = "release/lplot.lua"
     })
@@ -26,6 +27,6 @@ darwin.add_recipe({
     name="amalgamation",
     description="make a single file amalgamation of the project",
     outs={"release/lplot.lua"},
-    inputs={},
+    inputs={"src"},
     callback=amalgamation_build
 })
