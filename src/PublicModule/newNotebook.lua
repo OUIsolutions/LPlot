94f3927c3c9de852fed557ddd8f0f11ca946f739
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-08 08:00:41 -0300

    att

--- src/PublicModule/newNotebook.lua
+++ src/PublicModule/newNotebook.lua
@@ -1,11 +1,15 @@
+PublicModuleObject.test = function ()
+    print(PrivateModuleObject.get_asset("teste.css"))
+end
+
 
 PublicModuleObject.newNotebook = function ()
     ---@type PROJECT_ALIASNotebook
     local selfobj = {}
     selfobj.content = {}
-    selfobj.add_text = function (text) return PrivateModule.NotebookMethod_add_text(selfobj, text) end
-    selfobj.add_title = function (title) return PrivateModule.NotebookMethod_add_title(selfobj, title) end
-    selfobj.add_table = function (data) return PrivateModule.NotebookMethod_add_table(selfobj, data) end
-    selfobj.get_html = function () return PrivateModule.NotebookMethod_get_html(selfobj) end
+    selfobj.add_text = function (text) return PrivateModuleObject.NotebookMethod_add_text(selfobj, text) end
+    selfobj.add_title = function (title) return PrivateModuleObject.NotebookMethod_add_title(selfobj, title) end
+    selfobj.add_table = function (data) return PrivateModuleObject.NotebookMethod_add_table(selfobj, data) end
+    selfobj.get_html = function () return PrivateModuleObject.NotebookMethod_get_html(selfobj) end
     return selfobj
 end
---
+++ todo.md
@@ -0,0 +1,6 @@
+
+- create the asset mechanic 
+- Create the documentation struct 
+- create the internal template mechanic fo readme 
+- create the sample mechanic 
+- improve CLAUDE.md orientating how to setup a new public function
