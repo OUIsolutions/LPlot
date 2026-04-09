e8b3e0d26fdf76ad4351cb5a276ed29d05232924
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-09 00:47:41 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -2,5 +2,11 @@
 
 
 PROJECT_NAME = "lplot"
+REPLACERS = {
+    REQUIRED_DARWIN_VERSION = "0.020",
+    PROJECT_NAME = PROJECT_NAME,
+    PROJECT_REPO = "https://github.com/OUIsolutions/LPlot",
+    PROJECT_VERSION = "0.0.1",
+}
 
 darwin.load_all("builds")
