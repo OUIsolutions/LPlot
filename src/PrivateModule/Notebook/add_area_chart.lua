797fccf9a62018a70f782c1360835aead4d19733
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-09 10:34:50 -0300

    changing names

--- src/PrivateModule/Notebook/add_area_chart.lua
+++ src/PrivateModule/Notebook/add_area_chart.lua
@@ -1,5 +1,5 @@
 
-PrivateModuleObject.Notebook_add_area_chart = function(selfobj, data, options)
+PrivateModuleObject.Notebook___add_area_chart = function(selfobj, data, options)
     if not data or type(data) ~= "table" then return selfobj end
 
     options = options or {}
